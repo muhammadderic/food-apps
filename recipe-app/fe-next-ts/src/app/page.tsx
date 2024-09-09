7b20c34f7e37462934ba322c7e0867884ad0da0e
muhammadderic <muhammad.donny.e@gmail.com>
2024-09-09 20:33:26 +0700

    Update home page

--- recipe-app/fe-next-ts/src/app/page.tsx
+++ recipe-app/fe-next-ts/src/app/page.tsx
@@ -1,5 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <h1>Hello Deric</h1>
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-gray-100">
+      <div className="relative">
+        <h1 className="text-6xl font-bold text-center p-8 text-orange-700 bg-opacity-20">
+          Welcome to My Recipe App
+        </h1>
+        <p className="absolute bottom-0 right-0 text-sm text-gray-500">
+          updated by muhammadderic
+        </p>
+      </div>
+
+      <Link
+        href="/"
+        className="mt-8 px-6 py-3 bg-orange-200 text-orange-700 font-semibold rounded-lg shadow-lg hover:shadow-none transition-shadow"
+      >
+        Explore Recipes
+      </Link>
+    </div>
+
   );
 }
